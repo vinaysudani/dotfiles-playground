4cd8e5e2238fed41a5f577f2baf50b4247ac6f3a
Vinay Sudani <vinaysudani9@gmail.com>
2026-04-03 06:56:46 +0000

    Add neovim config

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+	-- { "echasnovski/mini.pairs", enabled = false },
+	{ "nvim-mini/mini.pairs", enabled = false },
+	{ "folke/noice.nvim", enabled = false },
+	{ "rcarriga/nvim-notify", enabled = false },
+}
